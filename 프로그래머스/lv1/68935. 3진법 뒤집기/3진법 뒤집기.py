cad923cc8671506f1fb665b4b5b8fab027d8e3a3
jinseong1170 <131628919+jinseong1170@users.noreply.github.com>
2023-05-05 10:02:06 +0900

    Update 3진법 뒤집기.py

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,10 +1,22 @@
-# 다른 풀이
-def solution(n):
-    tmp = ''
-    
-    while n:
-        tmp += str(n % 3)
-        n = n // 3
+# 블로그 참고 다른 풀이
 
-    answer = int(tmp, 3)
+# 1. 삼진법을 계산하기 위해서 %3으로 나머지를 구합니다.
+# 2. 낮은 숫자부터 1, 3, 9, 27을 곱하기 위해 arr을 역순으로 정렬합니다.
+# 3. 십진법으로 다시 변환합니다.
+
+def solution(n):
+    answer = 0
+    lst = ""    # 삼진법을 임시 저장할 문자열
+
+    while n > 0:    # 삼진법 계산
+        lst += str(n % 3)
+        n //= 3
+
+    lst = lst[::-1]     # 문자열을 반대로 정렬
+    count = 1
+
+    for i in lst:   # 다시 십진법으로 계산
+        answer += int(i) * count
+        count *= 3
+        
     return answer
