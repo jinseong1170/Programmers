ead7da7de782dacd50c258b4f1735783b8a9d197
jinseong1170 <131628919+jinseong1170@users.noreply.github.com>
2023-05-10 10:06:55 +0900

    [unrated] Title: 과일 장수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,8 @@
+# 다른 풀이
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    
+    for i in range(len(score) // m):
+        answer += score[i * m + m - 1] * m
+    return answer
